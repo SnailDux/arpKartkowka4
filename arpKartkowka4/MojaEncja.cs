4f82bab17832a57feadecc7cae9a7d3f751245aa
Przemysław Kudełka <235336@student.pwr.edu.pl>
2021-04-16 18:50:39 +0200

    Add project files.

---
+++ arpKartkowka4/MojaEncja.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arpKartkowka4
+{
+    public class MojaEncja
+    {
+        public int ID { set; get; }
+        public int Age { set; get; }
+    }
+}
---
+++ arpKartkowka4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+
+namespace arpKartkowka4
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var context = new MyDB();
+            Random rand = new Random();
+
+            for (int i = 0; i < 3; ++i)
+            {
+                context.MyTbl.Add(new MojaEncja {Age = rand.Next(99)}) ;
+                context.SaveChanges();
+            }
+
+            var toPrint = context.MyTbl.ToList();
+            toPrint.ForEach(x => Console.WriteLine("ID: " + x.ID + "\nAge: "+ x.Age + "\n"));
+
+            Console.ReadLine();
+        }
+    }
+}
